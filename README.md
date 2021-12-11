74acd45d03f6700d7d19a869ca2f4dedac5c9a86
Moondarker <lenayxa@gmail.com>
2021-12-11 21:29:45 +0300

    Initial release

---
+++ README.md
@@ -0,0 +1,4 @@
+# Better Book and Quill
+Originally developed as module for my utility client to use on anarchy servers, and is now free for anyone to use :)
+
+![Screenshot](https://i.imgur.com/idzj4z5.png)
---
+++ src/main/java/me/moondark/log4jsafetycheck/Log4jSafetyChecker.java
@@ -0,0 +1,14 @@
+package me.moondark.log4jsafetycheck;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "log4jsafetycheck", name = "Log4jSafetyChecker", version = Log4jSafetyChecker.VERSION)
+public final class Log4jSafetyChecker {
+	public static final String VERSION = "1.00";
+
+    @Mod.EventHandler
+    public void onPreInit(FMLPreInitializationEvent event) {
+        System.out.println("Log4j Safety Checker ready");
+    }
+}
