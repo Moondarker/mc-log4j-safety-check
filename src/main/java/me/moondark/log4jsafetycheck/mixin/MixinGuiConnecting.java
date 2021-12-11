25f3056d76a4470b8dd6995bec1fa1b8c9957ada
Moondarker <lenayxa@gmail.com>
2021-12-12 02:42:26 +0300

    More consistent this. usage

--- src/main/java/me/moondark/log4jsafetycheck/mixin/MixinGuiConnecting.java
+++ src/main/java/me/moondark/log4jsafetycheck/mixin/MixinGuiConnecting.java
@@ -17,14 +17,14 @@ import net.minecraft.util.text.TextComponentString;
 
 @Mixin(GuiConnecting.class)
 public abstract class MixinGuiConnecting {
+    private boolean log4jUnsafe = false;
+    private String log4jVersion = "";
+
     @Shadow
     private NetworkManager networkManager;
 
     @Shadow
     private boolean cancel;
-
-    private boolean log4jUnsafe = false;
-    private String log4jVersion = "";
 
     @Shadow
     private GuiScreen previousGuiScreen;
@@ -33,27 +33,27 @@ public abstract class MixinGuiConnecting {
     private void connect(final String ip, final int port, CallbackInfo callbackInfo) {
         Package log4jClass = org.apache.logging.log4j.Logger.class.getPackage();
 
-        log4jVersion = log4jClass.getImplementationVersion();
-        String[] log4jVersionSplit = log4jVersion.split("\\.");
+        this.log4jVersion = log4jClass.getImplementationVersion();
+        String[] log4jVersionSplit = this.log4jVersion.split("\\.");
 
         if (Integer.valueOf(log4jVersionSplit[0]) == 2 && Integer.valueOf(log4jVersionSplit[1]) < 15) {
             this.cancel = true;
-            log4jUnsafe = true;
+            this.log4jUnsafe = true;
 
             if (this.networkManager != null)
             {
                 this.networkManager.closeChannel(new TextComponentString("Aborted"));
             }
 
-            System.out.println("Outdated log4j version: v" + log4jVersion);
+            System.out.println("Outdated log4j version: v" + this.log4jVersion);
             callbackInfo.cancel();
         }
     }
 
     @Inject(method = "updateScreen", at = @At("RETURN"))
     public void updateScreen(CallbackInfo info) {
-        if (this.networkManager == null && cancel && log4jUnsafe) { // It's essentially impossible to switch displays in connect() because it's called in constructor, which is called before another displayGuiScreen()
-            Minecraft.getMinecraft().displayGuiScreen(new GuiDisconnected(previousGuiScreen, "connect.failed", new TextComponentString(ChatFormatting.RED + "[log4jSafetyCheck] " + ChatFormatting.RESET + "Outdated log4j version: v" + log4jVersion)));
+        if (this.networkManager == null && this.cancel && this.log4jUnsafe) { // It's essentially impossible to switch displays in connect() because it's called in constructor, which is called before another displayGuiScreen()
+            Minecraft.getMinecraft().displayGuiScreen(new GuiDisconnected(this.previousGuiScreen, "connect.failed", new TextComponentString(ChatFormatting.RED + "[log4jSafetyCheck] " + ChatFormatting.RESET + "Outdated log4j version: v" + this.log4jVersion)));
         }
     }
 }
